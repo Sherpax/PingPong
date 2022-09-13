6d2a7b9210ffe2e22d57916bbabdc47b80c9e16b
Sherpax <keepergold451@gmail.com>
2022-09-13 17:50:02 +0200

    First commit

---
+++ Ping Pong/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Ping_Pong {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.npc_raqueta = new System.Windows.Forms.PictureBox();
+            this.player_raqueta = new System.Windows.Forms.PictureBox();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.npc_raqueta)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player_raqueta)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // npc_raqueta
+            // 
+            this.npc_raqueta.BackColor = System.Drawing.Color.Red;
+            this.npc_raqueta.Location = new System.Drawing.Point(1194, 211);
+            this.npc_raqueta.Name = "npc_raqueta";
+            this.npc_raqueta.Size = new System.Drawing.Size(10, 106);
+            this.npc_raqueta.TabIndex = 3;
+            this.npc_raqueta.TabStop = false;
+            // 
+            // player_raqueta
+            // 
+            this.player_raqueta.BackColor = System.Drawing.Color.Red;
+            this.player_raqueta.Location = new System.Drawing.Point(1, 211);
+            this.player_raqueta.Name = "player_raqueta";
+            this.player_raqueta.Size = new System.Drawing.Size(10, 106);
+            this.player_raqueta.TabIndex = 2;
+            this.player_raqueta.TabStop = false;
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.Transparent;
+            this.ball.BackgroundImage = global::Ping_Pong.Properties.Resources.BALL;
+            this.ball.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ball.Image = global::Ping_Pong.Properties.Resources.BALL;
+            this.ball.Location = new System.Drawing.Point(1104, 56);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(44, 42);
+            this.ball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ball.TabIndex = 0;
+            this.ball.TabStop = false;
+            this.ball.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 20;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 20;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1206, 751);
+            this.Controls.Add(this.npc_raqueta);
+            this.Controls.Add(this.player_raqueta);
+            this.Controls.Add(this.ball);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.npc_raqueta)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player_raqueta)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox player_raqueta;
+        private System.Windows.Forms.PictureBox npc_raqueta;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+    }
+}
+
---
+++ Ping Pong/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Drawing; //Para dibujar la posición de la imagen
+using System.Windows.Forms;
+
+namespace Ping_Pong {
+    public partial class Form1 : Form {
+
+        private int speed = 5;
+        private int counter = 0;
+        private int player_points = 0;
+        private bool arriba, derecha;
+
+        private bool npcArriba, npcAbajo;
+        private bool cambioDireccion = false;
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private bool IsTouching(PictureBox p1, PictureBox p2) {
+            if (p1.Location.X + p1.Width < p2.Location.X)
+                return false;
+            if (p2.Location.X + p2.Width < p1.Location.X)
+                return false;
+            if (p1.Location.Y + p1.Height < p2.Location.Y)
+                return false;
+            if (p2.Location.Y + p2.Height < p1.Location.Y)
+                return false;
+            return true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) {
+
+            //if (this.ball.Right > this.npc_raqueta.Right) { //Ganar
+            //    timer1.Enabled = false;
+            //    MessageBox.Show("Has ganado");
+            //    this.speed = 5;
+            //    this.counter = 0;
+            //}
+
+            if (IsTouching(this.ball, this.player_raqueta)) {
+                this.derecha = true;
+            }
+
+            if (IsTouching(this.ball, this.npc_raqueta)) {
+                this.derecha = false;
+                Console.WriteLine("Tocado");
+            }
+
+            if (derecha) { //Va para la derecha
+                this.ball.Left += this.speed;
+            } else {
+                this.ball.Left -= this.speed;
+            }
+
+            if (arriba) { //Va para arriba
+                this.ball.Top += this.speed;
+            } else {
+                this.ball.Top -= this.speed;
+            }
+
+            if (this.ball.Top >= this.Height - 71) { //Si se pega en la pared de abajo
+                arriba = false;
+            }
+
+            if (this.ball.Top <= 0) { //Se pega en la pared de arriba
+                arriba = true;
+            }
+
+            if (this.ball.Right <= 0) { //Si pega en la pared de la izquierda
+                derecha = true;
+            }
+
+            if (this.ball.Left >= this.Width - 51) {
+                derecha = false;
+            }
+
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e) {
+            player_raqueta.Top = e.Y;
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            this.Text = "Puntuación: 0";
+
+            Random random = new Random();
+
+            npc_raqueta.Top = random.Next(200, 550);
+
+            this.ball.Location = new Point(55, random.Next(this.Height));
+
+            this.arriba = true;
+            this.derecha = true;
+            this.timer1.Enabled = true;
+
+            this.timer2.Enabled = true;
+            this.player_points = 0;
+
+        }
+
+        private void timer3_Tick(object sender, EventArgs e) {
+
+            npcAbajo = npc_raqueta.Top + this.npc_raqueta.Height <= this.Height - 76;
+
+            if (npcAbajo) {
+                this.npc_raqueta.Top += 20;
+            } else {
+                this.timer3.Enabled = false;
+                this.timer2.Enabled = true;
+            }
+        }
+
+        private void timer4_Tick(object sender, EventArgs e) {
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e) {
+            Console.WriteLine("Window: " + this.Height);
+
+            npcArriba = npc_raqueta.Top - this.npc_raqueta.Height >= -85;
+
+            if (npcArriba) {
+                this.npc_raqueta.Top -= 20;
+            } else {
+                this.timer2.Enabled = false;
+                this.timer3.Enabled = true;
+            }
+
+
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e) {
+
+        }
+    }
+}
